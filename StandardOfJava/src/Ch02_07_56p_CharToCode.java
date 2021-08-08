1acbae0fb0196e43d11a6ce3957db73b6c98ad8e
Yi <66saenggak99@gmail.com>
2021-08-08 19:10:55 +0900

    Chapter02_CharToCode_Casting

---
+++ StandardOfJava/src/Ch02_07_56p_CharToCode.java
@@ -0,0 +1,16 @@
+
+public class Ch02_07_56p_CharToCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';		// char ch = 65;
+		int code = (int)ch;		//ch에 저장된 값을 int타입으로 변환하여 저장
+		
+		System.out.printf("%c = %d(%#X) %n", ch, code, code);
+		
+		char hch = '가';		// char hch = 0xAC00;
+		System.out.printf("%c = %d(%#X) %n", hch, (int)hch, (int)hch);
+		
+	}
+
+}
